432352b27ae983da12f9b5621d229cffc1f951e9
naveenv20 <naveenv20@gmail.com>
2018-07-16 23:48:32 +0530

    Added base files

---
+++ src/test/java/com/qtpselenium/hybrid/AppTest.java
@@ -0,0 +1,14 @@
+package com.qtpselenium.hybrid;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+   @Test
+   public void testA() {
+	   System.out.println("Hello");
+	  
+   }
+   
+ 
+}
